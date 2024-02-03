c0610fa227e7fd8a2eb4f45ec4dcd3718a92959d
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-02-03 23:38:12 +0900

    Step Zero: JSX와 DOM Element

---
+++ Step/index.js
@@ -0,0 +1,17 @@
+const element = {
+	type: 'h1',
+	props: {
+		title: 'foo',
+		children: 'Hello',
+	},
+};
+
+const node = document.createElement(element.type);
+node['title'] = element.props.title;
+
+const text = document.createTextNode('');
+text['nodeValue'] = element.props.children; //props: {nodeValue: "hello"}.
+
+const container = document.getElementById('root');
+node.appendChild(text);
+container.appendChild(node);
