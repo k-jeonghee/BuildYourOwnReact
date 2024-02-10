9374f9478a8bcbfe9a523d2db346318b962d5a93
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-02-11 02:20:50 +0900

    feat: useState hook

---
+++ src/hooks/useState.js
@@ -0,0 +1,39 @@
+import { didactState } from '../core';
+
+//[didactState.hookIndex];
+export function useState(initial) {
+	const oldHook =
+		didactState.wipFiber.alternate &&
+		didactState.wipFiber.hooks &&
+		didactState.wipFiber.alternate.hooks[didactState.hookIndex];
+
+	const hook = {
+		state: oldHook ? oldHook.state : initial,
+		queue: [],
+	};
+
+	const actions = oldHook ? oldHook.queue : [];
+	actions.forEach((action) => {
+		hook.state = action(hook.state);
+	});
+
+	const setState = (action) => {
+		if (typeof action === 'function') {
+			hook.queue.push(action);
+		} else {
+			hook.queue.push(() => action);
+		}
+
+		didactState.wipRoot = {
+			dom: didactState.currentRoot.dom,
+			props: didactState.currentRoot.props,
+			alternate: didactState.currentRoot,
+		};
+		didactState.nextUnitOfWork = didactState.wipRoot;
+		didactState.deletions = [];
+	};
+
+	didactState.wipFiber.hooks.push(hook);
+	didactState.hookIndex++;
+	return [hook.state, setState];
+}
