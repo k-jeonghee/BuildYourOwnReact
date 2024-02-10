c514870e95f1b96fdd9efaf94d82848905825b22
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-02-11 02:19:24 +0900

    feat: Didact API

---
+++ src/didact.js
@@ -0,0 +1,10 @@
+import { createElement, render } from './core';
+import { useState } from './hooks/useState';
+
+const Didact = {
+	createElement,
+	render,
+	useState,
+};
+
+export default Didact;
