7b8584674501a2f510de478385b0a32870d8ca14
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-02-12 12:50:20 +0900

    refactor: hook 테스트를 위한 컴포넌트 코드 설정

--- src/components/ChildCounter.js
+++ src/components/ChildCounter.js
@@ -1,8 +1,17 @@
 import Didact from '../didact';
+import { useMemo } from '../hooks/useMemo';
+import { useState } from '../hooks/useState';
+import { useEffect } from '../hooks/useEffect';
 
-function ChildCounter(count) {
+function ChildCounter(count, memoizedCallback) {
 	console.log('Child');
-	return Didact.createElement('div', null, 'Child ', count);
+	const [num, setNum] = useState(5);
+	const memoizedValue = useMemo(() => {
+		console.log('useMemo compute 실행');
+		return num * 5;
+	}, []);
+	useEffect(() => memoizedCallback(), [memoizedCallback]);
+	return Didact.createElement('p', null, `Child: ${count} / MemoizedValue: ${memoizedValue}`);
 }
 
 export default Didact.memo(ChildCounter);
--- src/components/ParentCounter.js
+++ src/components/ParentCounter.js
@@ -1,18 +1,34 @@
 import Didact from '../didact.js';
+import { useEffect } from '../hooks/useEffect.js';
+import { useCallback } from '../hooks/useCallback';
 import ChildCounter from './ChildCounter.js';
 
 function ParentCounter() {
 	console.log('Parent');
 	const [count, setCount] = Didact.useState(1);
-	const [memo, setMemo] = Didact.useState(100);
+	const [count2, setCount2] = Didact.useState(100);
+	const memoizedCallback = useCallback(() => console.log(`useCallback 호출: ${count}`), [count]);
+	useEffect(() => {
+		console.log('useEffect 실행');
+	}, []);
 	return Didact.createElement(
 		'div',
-		{
-			onClick: () => setMemo((c) => c + 10),
-		},
-		'Count ',
-		memo,
-		ChildCounter(count)
+		null,
+		Didact.createElement(
+			'p',
+			{
+				onClick: () => setCount((c) => c + 10),
+			},
+			`Count: ${count}`
+		),
+		Didact.createElement(
+			'p',
+			{
+				onClick: () => setCount2((c) => c + 10),
+			},
+			`Count2: ${count2}`
+		),
+		ChildCounter(count, memoizedCallback)
 	);
 }
 
